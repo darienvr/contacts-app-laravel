7249892bd9cc14688296375b89e80b6b16b498ab
darienvr <118293353+darienvr@users.noreply.github.com>
2023-06-17 10:15:53 -0500

    row contacts show

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -12,25 +12,31 @@
         </div>
     </div>
 
-    <div>
-        @foreach ($contacts as $contact)
-            <div class="d-flex justify-content-center mt-5">
-                <div class="card" style="width: 300px;">
-                    <div class="card-body" style="width: 200px;">
+    <div class="d-flex justify-content-center mt-1">
+        <div class="row">
+        @foreach ($contacts as $counter => $contact)
+            @if ($counter % 3 == 0)
+                <div class="row">
+                </div>  
+            @endif
+            <div class="col-md-4 mt-3 d-flex justify-content-center" >
+                <div class="card text-center" style="width: 250px;">
+                    <div class="card-body align-items-center">
                         <h3>{{$contact->nombre}}</h3>
                         <h5>{{$contact->numero}}</h5>
                         <a href="{{route('contacts-edit', ['id'=>$contact->id])}}">
-                            <button class="btn btn-warning">Edit</button>
+                            <button class="btn btn-warning mb-2" style="width: 80px;">Edit</button>
                         </a>
                         <form action="{{ route('contacts-destroy', ['id'=>$contact->id])}}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-danger">Delete</button>
+                            <button class="btn btn-danger" style="width: 80px;">Delete</button>
                         </form>
                     </div>
                 </div>
             </div>
         @endforeach
+        </div>
     </div>
 
 @endsection
